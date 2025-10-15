1520e3a1df799cc6a282ec9c3c85492dc92cf3d0
Dohyeon Sagong <dsagong@c1r21s5.42gyeongsan.kr>
2025-10-15 13:05:29 +0900

    3

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@ int main(void)
     Fixed const b(10);
     Fixed const c(42.42f);
     Fixed const d(b);
-    a = Fixed(1234.4321f);
+    a = Fixed(1234.55555555555555555555555555555555f);
 
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
@@ -17,6 +17,11 @@ int main(void)
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+    std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+    std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+    std::cout << "d is " << d.toFloat() << " as float" << std::endl;
 
     return 0;
 }
